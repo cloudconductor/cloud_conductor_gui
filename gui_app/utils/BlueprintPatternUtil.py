bf301a16086e13aecf0623cd2788f50d019fe2c6
ryota <sakamoto.ryota@tis.co.jp>
2016-02-24 13:02:44 +0900

    Add the #format_pattern

--- gui_app/utils/BlueprintPatternUtil.py
+++ gui_app/utils/BlueprintPatternUtil.py
@@ -1,6 +1,7 @@
 from ..utils import ApiUtil
 from ..utils import StringUtil
 from ..utils.ApiUtil import Url
+from itertools import chain
 
 
 def get_blueprint_pattern_list(code, token, blueprint_id):
@@ -95,7 +96,6 @@ def delete_blueprint_pattern(code, token, id, pattern_id):
 
     ApiUtil.requestDelete(url, code, data)
 
-
 def add_blueprint_pattern_list(code, token, id, pt_list, id_list):
     if StringUtil.isEmpty(id):
         return None
@@ -110,7 +110,6 @@ def add_blueprint_pattern_list(code, token, id, pt_list, id_list):
     for pt in pt_list:
         add_blueprint_pattern(code, token, id, pt.get('id'),
                               pt.get('revison'), pt.get('os_version'))
-
 
 def delete_blueprint_pattern_list(code, token, id, pt_list, id_list):
     if StringUtil.isEmpty(id):
@@ -142,3 +141,29 @@ def dic_pattern_list(patterns, ids):
             lists.append(pt)
 
     return lists
+
+def format_pattern(platforms, platform_versions, revisions, ids):
+    dict_platforms = StringUtil.stringToDictList(platforms)
+    dict_platform_versions = StringUtil.stringToDictList(platform_versions)
+    dict_revisions = StringUtil.stringToDictList(revisions)
+
+    dict_platforms = [x for x in dict_platforms if str(x['id']) in ids]
+    dict_platform_versions = [x for x in dict_platform_versions if str(x['id']) in ids]
+    dict_revisions = [x for x in dict_revisions if str(x['id']) in ids]
+
+    result = []
+    for id in ids:
+        t_platform = {}
+        t_platform_version = {}
+        t_revision = {}
+        for platform, platform_version, revision in zip(dict_platforms, dict_platform_versions, dict_revisions):
+            if id == str(platform['id']):
+                t_platform = platform
+            if id == str(platform_version['id']):
+                t_platform_version = platform_version
+            if id == str(revision['id']):
+                t_revision = revision
+
+        result.append(dict(chain(t_platform.items(), t_platform_version.items(), t_revision.items())))
+
+    return result
